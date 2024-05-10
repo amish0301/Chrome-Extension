140a3cd8e0c60389fa4cff4503e7399ee8e33bb6
amish <pithvaamish76@gmail.com>
2024-05-10 23:16:40 +0530

    basic setup done

--- background.js
+++ background.js
@@ -1,41 +1,48 @@
-let isWater = document.getElementById("water_break");
-
 // CONTEXT MENUS - CREATE
-function createContextMenus() {
-  chrome.contextMenus.create({
-    id: "water",
-    title: "Water Reminder",
-    contexts: ["selction"],
-  });
-  chrome.contextMenus.create({
-    id: "nap",
-    title: "Nap Reminder",
-    contexts: ["selection"],
-  });
+function createContextMenu() {
+  chrome.contextMenus.create(
+    {
+      id: "reminder",
+      title: "Set Your Reminder",
+      contexts: ["all"],
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.log("Error in creating context menu", chrome.runtime.lastError);
+      } else {
+        chrome.storage.local.set({ menuItemId: "reminder" }, () => {
+          if (chrome.runtime.lastError) {
+            console.error(
+              "Error storing context menu ID: " +
+                chrome.runtime.lastError.message
+            );
+          } else {
+            console.log("Context menu ID stored");
+          }
+        });
+      }
+    }
+  );
 }
 
-function createAlarms(name) {
-  chrome.alarms.create("water", { periodInMinutes: 1 });
-}
+createContextMenu();
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    console.log("water :", isWater);
-    // console.log(info);
-})
-
-chrome.runtime.onInstalled.addListener(({ reason }) => {
-  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    console.log("Installed Extension");
-    createContextMenus();
-  } else if (reason === chrome.runtime.OnInstalledReason.UPDATE) {
+  if (info.menuItemId === "reminder") {
+    chrome.windows.create({
+      url: "popup.html",
+      type: "popup",
+      width: 370,
+      height: 325,
+    });
   }
 });
 
+function createAlarms(name) {
+  console.log("Alarm created", name);
+}
 
-/*
-
-Checks needed:
-1. if CREATED then check [contexts values]
-2. Check isWater Value
-
-*/
+// create context on Refresh
+chrome.runtime.onInstalled.addListener(() => {
+  createContextMenu();
+});
