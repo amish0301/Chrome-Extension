efff8779824684c4c87ca95160eff75535d2c290
amish <pithvaamish76@gmail.com>
2024-05-12 22:54:07 +0530

    notification testing done

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 // CONTEXT MENUS - CREATE
+let prevTimeOfWater = 0;
+let prevTimeOfNap = 0;
+
 function createContextMenu() {
   chrome.contextMenus.create(
     {
@@ -6,7 +9,7 @@ function createContextMenu() {
       title: "Set Your Reminder",
       contexts: ["all"],
     },
-    () => {
+    async () => {
       if (chrome.runtime.lastError) {
         console.log("Error in creating context menu", chrome.runtime.lastError);
       } else {
@@ -25,8 +28,6 @@ function createContextMenu() {
   );
 }
 
-createContextMenu();
-
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "reminder") {
     chrome.windows.create({
@@ -38,11 +39,69 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   }
 });
 
-function createAlarms(name) {
-  console.log("Alarm created", name);
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "water") {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "Water Reminder",
+      message: "Time to drink water!",
+    });
+  }
+
+  if (alarm.name === "nap") {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "Nap Reminder",
+      message: "Time to take a nap!",
+    });
+  }
+});
+
+async function createAlarms({ time, type }) {
+  if (type === "water") {
+    if (prevTimeOfWater === time) {
+      return;
+      // i can send message to the client
+    }
+
+    chrome.alarms.clear("water");
+    // make new alarm
+    await chrome.alarms.create(
+      type,
+      {
+        delayInMinutes: time,
+        periodInMinutes: time,
+      },
+      () => {
+        if (chrome.runtime.lastError)
+          console.log("Error in creating alarm", chrome.runtime.lastError);
+        else prevTimeOfWater = time;
+      }
+    );
+  }
+
+  if (type === "nap") {
+  }
 }
 
-// create context on Refresh
+function stopAlarm(type) {
+  chrome.alarms.clear(type);
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.time) {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "https://cdn-icons-png.flaticon.com/512/1792/1792931.png",
+      title: "Test",
+      message: "Hey there!!",
+    })
+  }
+});
+
+// create context menu
 chrome.runtime.onInstalled.addListener(() => {
   createContextMenu();
 });
--- popup.js
+++ popup.js
@@ -1,26 +1,21 @@
 const nap_break_input = document.getElementById("nap_break");
 const water_break_input = document.getElementById("water_break");
 
-if (water_break_input) {
-  water_break_input.addEventListener("change", function () {
-    if (water_break_input.checked) {
-      document.getElementsByClassName("expand_water")[0].style.display =
-        "block";
-    } else {
-      document.getElementsByClassName("expand_water")[0].style.display = "none";
-    }
-  });
-}
+water_break_input.addEventListener("change", function () {
+  if (water_break_input.checked) {
+    document.getElementsByClassName("expand_water")[0].style.display = "block";
+  } else {
+    document.getElementsByClassName("expand_water")[0].style.display = "none";
+  }
+});
 
-if (nap_break_input) {
-  nap_break_input.addEventListener("change", function () {
-    if (nap_break_input.checked) {
-      document.getElementsByClassName("expand_nap")[0].style.display = "block";
-    } else {
-      document.getElementsByClassName("expand_nap")[0].style.display = "none";
-    }
-  });
-}
+nap_break_input.addEventListener("change", function () {
+  if (nap_break_input.checked) {
+    document.getElementsByClassName("expand_nap")[0].style.display = "block";
+  } else {
+    document.getElementsByClassName("expand_nap")[0].style.display = "none";
+  }
+});
 
 const collapseBtn = document.getElementsByClassName("collapsible")[0];
 collapseBtn.addEventListener("click", function () {
@@ -37,7 +32,6 @@ const collapseBtn2 = document.getElementsByClassName("collapsible2")[0];
 collapseBtn2.addEventListener("click", function () {
   // this.classList.toggle("active");
   const nap_content = document.getElementsByClassName("nap_content")[0];
-  // NAP CONTENT
   if (nap_content.style.display === "block") {
     nap_content.style.display = "none";
   } else {
@@ -46,15 +40,28 @@ collapseBtn2.addEventListener("click", function () {
 });
 
 function submit() {
-  const time_water = document.getElementsByName("water_break_input")[0].value;
+  let time_water = document.getElementsByName("water_break_input")[0].value;
   console.log(time_water);
 
-  const time_nap = document.getElementsByName("nap_break_input")[0].value;
+  let time_nap = document.getElementsByName("nap_break_input")[0].value;
   console.log(time_nap);
 
-  // TODO: send time_water and time_nap to server
+  // PENDING: send time_water and time_nap to server
+  async function sendMessage({ type, time }) {
+    try {
+      await chrome.runtime.sendMessage({
+        time,
+        type,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  if (time_water) sendMessage({ type: "water", time: parseInt(time_water) });
 
   // Clearing inputs
   document.getElementsByName("water_break_input")[0].value = "";
   document.getElementsByName("nap_break_input")[0].value = "";
 }
+
+document.getElementById("submit_btn").addEventListener("click", submit);
